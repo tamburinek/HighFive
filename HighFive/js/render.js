e0b70f396f84604c0edf00607d66140998d9999f
norbaart <norbaart@fel.cvut.cz>
2022-04-26 00:32:32 +0200

    generate skills

--- HighFive/js/render.js
+++ HighFive/js/render.js
@@ -1,1 +1,44 @@
+$(document).on("keydown",function(event) {
+    return event.key != "Enter";
+});
+
+let eli = document.getElementById("delete-button-basic-info");
+eli.addEventListener("click", (e)=>{
+    e.preventDefault();
+    let form = document.getElementById("form");
+    let basic = document.getElementById("basic");
+    let buttonRemove = document.getElementById("basic-remove-button");
+    form.removeChild(basic);
+    form.removeChild(buttonRemove);
+});
+
+let skillButton = document.getElementById("skill-button");
+skillButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let helperInput = document.getElementById("new-skill-title");
+    let textString = helperInput.value;
+    let formToTag = document.querySelector(".skill-tags");
+
+    if(textString == ''){
+        return;
+    }
+
+    liEl = document.createElement('li');
+
+    inputElementik = document.createElement('input');
+    inputElementik.type = "checkbox";
+    inputElementik.id = textString;
+    inputElementik.value = textString;
+
+    labelEl = document.createElement('label');
+    labelEl.htmlFor = textString;
+    labelEl.innerText = textString;
+
+    liEl.appendChild(inputElementik);
+    liEl.appendChild(labelEl);
+
+    formToTag.appendChild(liEl);
+
+    helperInput.value = '';
+})
 
