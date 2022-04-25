80091bdce9ee4610b749c83391e8406f4741ceff
norbaart <norbaart@fel.cvut.cz>
2022-04-25 23:47:07 +0200

    render pdf from canvas

--- HighFive/js/bottom.js
+++ HighFive/js/bottom.js
@@ -13,20 +13,22 @@ function volumeChanger() {
 
 window.addEventListener('resize', changeCanvasSize, true);
 function changeCanvasSize() {
-    var canvas = document.querySelector("svg.canvas");
+    var canvas = document.querySelector("canvas.canvas");
     var cHeight = canvas.clientHeight;
     var cWidthSize = canvas.style.width = (210/297)*cHeight + "px";
 }
 
-let test = document.getElementById("skill-button");
-test.addEventListener("click", (e) => {
-    e.preventDefault();
-})
 
-let test2 = document.getElementById("section-button");
-test2.addEventListener("click", (e) => {
-    e.preventDefault();
-})
+
+// let test = document.getElementById("skill-button");
+// test.addEventListener("click", (e) => {
+//     e.preventDefault();
+// })
+
+// let test2 = document.getElementById("section-button");
+// test2.addEventListener("click", (e) => {
+//     e.preventDefault();
+// })
 
 // let test3 = document.getElementById("test-drop");
 // test3.style.mask = "url(../resources/image004.png) no-repeat center / contain"
---
+++ HighFive/js/pdf.js
@@ -0,0 +1,25 @@
+function ExportPdf(){ 
+    kendo.drawing
+        .drawDOM("#myCanvas", 
+        { 
+            forcePageBreak: ".page-break", 
+            paperSize: "A4",
+            margin: { top: "1cm", bottom: "1cm" },
+            scale: 0.8,
+            height: 500, 
+            template: $("#page-template").html(),
+            keepTogether: ".prevent-split"
+        })
+            .then(function(group){
+            kendo.drawing.pdf.saveAs(group, "Test.pdf")
+        });
+    }
+
+let el = document.getElementById("index-button");
+el.addEventListener("click", ExportPdf);    
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.font = "10px Arial";
+ctx.fillText("Hello World", 200, 200);
+
