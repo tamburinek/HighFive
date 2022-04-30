d1f3073015aae9499c3f2c69df02267e7a82691d
norbaart <norbaart@fel.cvut.cz>
2022-04-30 21:44:26 +0200

    update

--- HighFive/js/index.js
+++ HighFive/js/index.js
@@ -1,7 +1,8 @@
 //animating home page
 function typeWriter(i) {
     let speed = 75;
-    var txt = 'HighFive is effective CV creater and generator. CV is the most important thing when you are looking for new job.';
+    var txt = `HighFive is effective CV creater and generator. CV is the most important thing when you are looking for new job.
+    So u have to spend some time to make it perfect. This app is actually not that perfect, but it is what it is.`;
     if (i < txt.length) {
         document.querySelector(".index-title").innerHTML += txt.charAt(i);
         setTimeout(typeWriter.bind(null, i+=1), speed);
