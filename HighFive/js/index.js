f5e2b82b2efe134366f737477fce01ab160081be
norbaart <norbaart@fel.cvut.cz>
2022-05-03 00:29:19 +0200

    transition done

--- HighFive/js/index.js
+++ HighFive/js/index.js
@@ -18,8 +18,12 @@ buttonToToggle.addEventListener("click", (e) => {
     let toglik = document.querySelector(".transition");
     // toglik.style.background = "white"
     let mainik = document.querySelector(".index-title");
+    let logik = document.querySelector(".logoLogo")
+    logik.style.width = "80vh";
     buttonToToggle.style.display = "none"
     mainik.style.display = "none"
+    // buttonToToggle.style.visibility = "hidden"
+    // mainik.style.visibility = "hidden"
 
     toglik.classList.toggle("anim-trans");
     console.log("i was here")
