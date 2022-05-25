967b4bf31034b4c674cd5adeb4ffce8430c7d1ae
norbaart <norbaart@fel.cvut.cz>
2022-05-25 21:58:28 +0200

    big checkpoint

--- HighFive/js/canvas.js
+++ HighFive/js/canvas.js
@@ -33,15 +33,36 @@ function drawRec(){
         }else {
             ctx.fillStyle = "#ffffff"
         }
-        ctx.font = "30px Calibri";
-        ctx.fillText("Skills:", cWidthSize / 8, cHeight /2.5);
 
+        drawInputs();
         drawSkills();
         drawSections();
 
         //profile pic
         ctx.drawImage(imageToCanvas, cWidthSize / 22, cHeight /15, imageToCanvas.width *2, imageToCanvas.height*2)
     }
+}
+
+function drawInputs(){
+    ctx.fillStyle = "#000000"
+    ctx.font = "bold 20px Calibri";
+    ctx.fillText("Job title:", cWidthSize / 2.7, cHeight /18);
+    ctx.fillText("Name:", cWidthSize / 2.7, cHeight /12);
+    ctx.fillText("Surname:", cWidthSize / 1.5, cHeight/12);
+    ctx.fillText("Email:", cWidthSize / 2.7, cHeight /6);
+    ctx.fillText("Phone:", cWidthSize / 1.5, cHeight /6);
+    ctx.fillText("Birth:", cWidthSize / 2.7, cHeight /4);
+    ctx.fillText("Country:", cWidthSize / 1.5, cHeight /4);
+
+    ctx.font = "20px Calibri";
+    ctx.fillText(document.getElementById("job-title-input").value, cWidthSize / 2.15, cHeight /18);
+    ctx.fillText(document.getElementById("first-name-input").value, cWidthSize / 2.7, cHeight /10);
+    ctx.fillText(document.getElementById("last-name-input").value, cWidthSize / 1.5, cHeight /10);
+    ctx.fillText(document.getElementById("email-input").value, cWidthSize / 2.7, cHeight /5.4);
+    ctx.fillText(document.getElementById("phone-input").value, cWidthSize / 1.5, cHeight /5.4);
+    ctx.fillText(document.getElementById("birth-input").value, cWidthSize / 2.7, cHeight /3.75);
+    ctx.fillText(document.getElementById("placeholder-input").value, cWidthSize / 1.5, cHeight /3.75);
+
 }
 
 function drawSkills(){
@@ -51,7 +72,8 @@ function drawSkills(){
     }else {
         ctx.fillStyle = "#ffffff"
     }
-
+    ctx.font = "28px Calibri";
+    ctx.fillText("Skills:", cWidthSize / 8, cHeight /2.5);
     ctx.font = "24px Calibri";
     for (const skill of initialState.skills) {
         if(skill.checked){
@@ -66,7 +88,7 @@ function drawSections(){
     ctx.fillStyle = "#000000"
     for (const skill of initialStateSection.sections) {
         let justForNow = document.getElementById(skill.text + "-area")
-        ctx.font = "bold 30px Calibri";
+        ctx.font = "bold 28px Calibri";
         ctx.fillText(skill.text, (cWidthSize / 2.7), (cHeight /2.8) + helperHeight);
         ctx.font = "20px Calibri";
         for (let index = 0; index < 8; index++) {
